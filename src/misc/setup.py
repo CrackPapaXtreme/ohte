ef9a95dac63fc65ff7e9bf247d2e3ae2698f3e15
CrackPapaXtreme <stathanael@proton.me>
2022-12-19 01:04:00 +0200

    update

--- src/misc/setup.py
+++ src/misc/setup.py
@@ -12,17 +12,18 @@ def format_program():
     try:
         rmtree(src("data"))
     except FileNotFoundError:
-        mkdir(src("data"))
-        mkdir(src("data/games"))
-        with open(src("data/users.json"), "w", encoding="utf-8") as usersjson:
-            usersjson.write("[]")
-            usersjson.close()
-        with open(src("data/adminpassword.txt"), "w", encoding="utf-8") as adminpasswordfile:
-            adminpasswordfile.write("yay")
-            adminpasswordfile.close()
-        with open(src("data/usernameblacklist.txt"), "w", encoding="utf-8") as blacklist:
-            blacklist.write("admin")
-            blacklist.close()
+        pass
+    mkdir(src("data"))
+    mkdir(src("data/games"))
+    with open(src("data/users.json"), "w", encoding="utf-8") as usersjson:
+        usersjson.write("[]")
+        usersjson.close()
+    with open(src("data/adminpassword.txt"), "w", encoding="utf-8") as adminpasswordfile:
+        adminpasswordfile.write("yay")
+        adminpasswordfile.close()
+    with open(src("data/usernameblacklist.txt"), "w", encoding="utf-8") as blacklist:
+        blacklist.write("admin")
+        blacklist.close()
 
 
 # This is here so that the function gets executes with poetry run invoke setup
